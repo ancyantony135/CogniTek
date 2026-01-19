46a264dc23be8c87f68198803de76272d9aee27b
ElvinAlias <aliaselvin@gmail.com>
2026-01-19 23:06:26 +0530

    Update api.js

--- Frontend/cognitek_web/src/api/api.js
+++ Frontend/cognitek_web/src/api/api.js
@@ -1,1 +1,16 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:8000/api",
+});
+
+export const fetchTasks = () => API.get("/tasks");
+
+export const uploadAudio = (formData) =>
+  API.post("/process-audio", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+export const updateTask = (id, is_completed) =>
+  API.patch(`/tasks/${id}`, { is_completed });
 
